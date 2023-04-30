604cafba9bdb6fec4379a0f313a2433002602b5d
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-04-29 21:12:25 -0700

    first commit

---
+++ functions_practice.py
@@ -0,0 +1,19 @@
+def hello():
+    print("Hello Master!")
+
+def pack(x, y, z):
+    my_list = [x, y, z]
+    return my_list
+
+def eat_lunch(lunch):
+    if len(lunch) == 0: #len() Return the length (the number of items) of an object
+        print("My lunchbox is empty!")
+    else:
+        print("First I eat", lunch[0])
+        for item in lunch[1:]:
+            print("Next I eat", item)
+            
+hello()
+print(pack(5, 31, 87))
+eat_lunch(["musubi", "loco moco"])
+

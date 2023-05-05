75f88bcd802d8b3d897821fe9cac96c42897fd48
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-05-04 21:49:26 -0700

    day 3 afterclass 1/2 done

--- day3_afterclass/paradigms.py
+++ day3_afterclass/paradigms.py
@@ -2,8 +2,7 @@
 # *! To check: python3 day3_afterclass/paradigms.py
 
 #* Functional Prompt
-'''
-# TODO 
+''' 
 Implement a function that will flatten and sort an array of integers in ascending order, and which adheres to a functional programming paradigm.
 
 Remember, when writing in a functional style:
@@ -11,25 +10,51 @@ Remember, when writing in a functional style:
  - Write only pure functions
  - Remember, functions may be higher order
 '''
-# ? answer here
+def flatten_and_sort(array):
+    # Initialize an empty list to store flattened elements
+    arr = []
+    # Iterate through each element in the input array
+    for item in array:
+        # If the element is a list, iterate through its elements and append them to arr
+        if isinstance(item, list):
+            for i in item:
+                arr.append(i)
+        # If the element is not a list, append it directly to arr
+        else:
+            arr.append(item)
+    # Return a new sorted list
+    return sorted(arr)
 
+#* Test 1
+test = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+res = flatten_and_sort(test)
+print(res)
+# output: [1, 2, 3, 4, 5, 6, 7, 8, 9] 
+#* Test 2
+test2 = [[1, 6, 8], [5, 3, 7], [5, 2, 9]]
+res2 = flatten_and_sort(test2)
+print(res2)
+# output: [1, 2, 3, 5, 5, 6, 7, 8, 9]
+#* Test 3
+test3 = [["a", "g", "n"], ["b", "t", "c"], ["f", "s", "z"]]
+res3 = flatten_and_sort(test3)
+print(res3)
+# output: ['a', 'b', 'c', 'f', 'g', 'n', 's', 't', 'z']
 
 '''
 # TODO 
 Once a functional solution to this problem has been implemented, answer the following questions.
- - How does this solution ensure data immutability?
-    #*
- - Is this solution a pure function? Why or why not?
-    #*
- - Is this solution a higher order function? Why or why not?
-    #*
- - Would it have been easier to solve this problem using a different programming style?
-    #*
- - Why in particular is functional programming a helpful paradigm when solving this problem?
-    #*
+ #? - How does this solution ensure data immutability?
+    #* It creates a list without modifying the original input list, ensures input list remains unchanged.
+ #? - Is this solution a pure function? Why or why not?
+    #* This solution is pure because it has no side effects, is deterministic, and has no external dependencies.
+ #? - Is this solution a higher order function? Why or why not?
+    #* This solution is not a higher-order function because it neither takes functions as arguments nor returns functions as output.
+ #? - Would it have been easier to solve this problem using a different programming style?
+    #* 
+ #? - Why in particular is functional programming a helpful paradigm when solving this problem?
+    #* 
 '''
-# ? answer here
-
 
 #* Object Oriented Prompt
 '''
@@ -46,13 +71,15 @@ Watto needs a new system for organizing his inventory of podracers. Help him do 
 '''
 # TODO
 Once an Object Oriented solution has been implemented, answer the following questions:
- - How does this solution demonstrate the four pillars of OOP? (It may not demonstrate all of them, describe only those that apply)
+ #? - How does this solution demonstrate the four pillars of OOP? (It may not demonstrate all of them, describe only those that apply)
     - Encapsulation
     - Abstraction
     - Inheritance
     - Polymorphism
- - Would it have been easier to implement a solution to this problem using a different coding style? Why or why not?
- - How in particular did Object Oriented Programming assist in the solving of this problem?
+ 
+ #? - Would it have been easier to implement a solution to this problem using a different coding style? Why or why not?
+ 
+ #? - How in particular did Object Oriented Programming assist in the solving of this problem?
 
 '''
 # ? answer here

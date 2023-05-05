d367bdf959f4430193533c10f261764f550de33a
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-05-04 23:12:14 -0700

    day2 activity 3

--- day2/activity3.py
+++ day2/activity3.py
@@ -3,6 +3,17 @@
 
 #* name_args - Accepts any number of named arguments and prints them in the pattern key : value one at a time.
 # TODO
+def name_args(**kwargs):
+    """
+    Accepts any number of named arguments and prints them in the pattern key : value one at a time.
+    """
+    # loop through each key-value pair in the kwargs dictionary
+    for key, value in kwargs.items():
+        # print the key-value pair in the format "key : value"
+        print(f"{key} : {value}")
+
+name_args(name='Allan', age=35, city='Honolulu')
+
 
 #* all_true - Returns True if all the elements in an iterable are true. Hint: there is an existing built-in function that could be very helpful here.
 # TODO

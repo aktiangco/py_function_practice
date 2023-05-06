4e616cfed5b8eecf8a04dc96911b638a663705c2
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-05-05 20:03:01 -0700

    day 3 afterclass 2/2 done

--- day3_afterclass/paradigms.py
+++ day3_afterclass/paradigms.py
@@ -25,21 +25,18 @@ def flatten_and_sort(array):
     # Return a new sorted list
     return sorted(arr)
 
-#* Test 1
+#* Testing
 test = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 res = flatten_and_sort(test)
-print(res)
-# output: [1, 2, 3, 4, 5, 6, 7, 8, 9] 
-#* Test 2
+print(res) # Output: [1, 2, 3, 4, 5, 6, 7, 8, 9] 
+
 test2 = [[1, 6, 8], [5, 3, 7], [5, 2, 9]]
 res2 = flatten_and_sort(test2)
-print(res2)
-# output: [1, 2, 3, 5, 5, 6, 7, 8, 9]
-#* Test 3
+print(res2) # Output: [1, 2, 3, 5, 5, 6, 7, 8, 9]
+
 test3 = [["a", "g", "n"], ["b", "t", "c"], ["f", "s", "z"]]
 res3 = flatten_and_sort(test3)
-print(res3)
-# output: ['a', 'b', 'c', 'f', 'g', 'n', 's', 't', 'z']
+print(res3) # Output: ['a', 'b', 'c', 'f', 'g', 'n', 's', 't', 'z']
 
 '''
 # TODO 
@@ -58,28 +55,70 @@ Once a functional solution to this problem has been implemented, answer the foll
 
 #* Object Oriented Prompt
 '''
-#  TODO
 Watto needs a new system for organizing his inventory of podracers. Help him do this by implementing an Object Oriented solution according to the following criteria.
  - First, he'll need a general Podracer class defined with max_speed, condition (perfect, trashed, repaired) and price attributes.
  - Define a repair() method that will update the condition of the podracer to "repaired".
  - Define a new class, AnakinsPod that inherits the Podracer class, but also contains a special method called boost that will multiply max_speed by 2.
  - Define another class that inherits Podracer and call this one SebulbasPod. This class should have a special method called flame_jet that will update the condition of another podracer to "trashed".
 '''
-# ? answer here
+ # - First, he'll need a general Podracer class defined with max_speed, condition (perfect, trashed, repaired) and price attributes.
+class Podracer:
+    def __init__(self, max_speed, condition, price):
+        self.max_speed = max_speed
+        self.condition = condition
+        self.price = price
+    
+    # - Define a repair() method that will update the condition of the podracer to "repaired".
+    def repair(self):
+        self.condition = "repaired"
+        
+#  - Define a new class, AnakinsPod that inherits the Podracer class.
+class AnakinsPod(Podracer):
+    def __init__(self, max_speed, condition, price):
+        super().__init__(max_speed, condition, price) # "super()" built-in function that allows a class to refer to its parent class
+    # special method called boost that will multiply max_speed by 2
+    def boost(self):
+        self.max_speed *= 2   # "*=" multiple 
+        
+# - Define another class that inherits Podracer and call this one SebulbasPod.
+class SebulbasPod(Podracer):
+    def __init__(self, max_speed, condition, price):
+        super().__init__(max_speed, condition, price)
+    # special method called flame_jet that will update the condition of another podracer to "trashed
+    def flame_jet(self, other):
+        other.condition = "trashed"
+        
+#* Testing
+# Create an instance of AnakinsPod
+anakin_pod = AnakinsPod(max_speed=100, condition="perfect", price=1000)
+
+# Create an instance of SebulbasPod
+sebulba_pod = SebulbasPod(max_speed=80, condition="perfect", price=800)
+
+# Call the boost method on AnakinsPod
+anakin_pod.boost()
+print(f"Anakin has utilized his boost to catch up with Sebulda and is now reaching a speed of {anakin_pod.max_speed}mph" ) # Output: Anakin has utilized his boost to catch up with Sebulda and is now reaching a speed of 200mph.
+
+# Call the flame_jet method on SebulbasPod, passing in AnakinsPod as an argument
+sebulba_pod.flame_jet(anakin_pod)
+print(f"Sebulda {anakin_pod.condition} Anakin's Pod racer") # Output: Sebulda trashed Anakin's Pod racer
+
+# Call the repair method on AnakinsPod
+anakin_pod.repair()
+print(f"Anakin's Pod racer has been {anakin_pod.condition}") # Output: Anakin's Pod racer has been repaired
 
 
 '''
-# TODO
 Once an Object Oriented solution has been implemented, answer the following questions:
  #? - How does this solution demonstrate the four pillars of OOP? (It may not demonstrate all of them, describe only those that apply)
     - Encapsulation
     - Abstraction
     - Inheritance
     - Polymorphism
- 
+    #* The OOP solution demonstrates the pillars of Encapsulation, Abstraction, Inheritance, and Polymorphism. Encapsulation is shown through the use of class methods and attributes, Abstraction through the hiding of internal implementation details, Inheritance through the creation of subclasses, and Polymorphism through the ability to use the same method on different objects with different results
  #? - Would it have been easier to implement a solution to this problem using a different coding style? Why or why not?
- 
+    #* Depending on the particular issue and the programmer's expertise with various coding styles, it may or may not have been simpler to create a solution to this problem using a different coding style.
  #? - How in particular did Object Oriented Programming assist in the solving of this problem?
-
+    #* By offering a method to group related information and actions into classes and objects, enabling the encapsulation and abstraction of implementation specifics, and promoting code reuse through inheritance and polymorphism, OOP helped to solve this issue.
 '''
-# ? answer here
+
